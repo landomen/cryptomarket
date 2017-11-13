dee7fe2674d1a0c81218387800b322e5e9f84b81
Domen Lanišnik <d.lanisnik@sportradar.com>
2017-11-13 20:07:04 +0100

    Initial project.

---
+++ app/src/main/java/si/lanisnik/cryptomarket/CryptoListActivity.java
@@ -0,0 +1,13 @@
+package si.lanisnik.cryptomarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CryptoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto_list);
+    }
+}
