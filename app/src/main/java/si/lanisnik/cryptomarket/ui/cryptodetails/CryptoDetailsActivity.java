dd94991df7c1166fcb861546ec9789248c5a79e0
Domen Lanišnik <domen.lanisnik@gmail.com>
2017-11-14 23:13:43 +0100

    Created CryptoDetails activity.

---
+++ app/src/main/java/si/lanisnik/cryptomarket/ui/cryptodetails/CryptoDetailsActivity.java
@@ -0,0 +1,20 @@
+package si.lanisnik.cryptomarket.ui.cryptodetails;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import si.lanisnik.cryptomarket.R;
+
+/**
+ * Created by Domen Lanišnik on 14/11/2017.
+ * domen.lanisnik@gmail.com
+ */
+public class CryptoDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto_details);
+    }
+}
